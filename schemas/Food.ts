c988de51b798a45fbb0094d9488b0b7cb12317be
MohaK <mohakhalili@outlook.com>
2025-01-20 20:00:39 +0100

    added zod and created food schema

---
+++ schemas/Food.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+const schema = z.object({
+  id: z.string().optional(),
+  name: z.string().min(1, { message: "Name is required" }),
+  categoryId: z.string().min(1, { message: "You must select a category" }),
+  numberInStock: z
+    .number({ invalid_type_error: "Stock must be a number" })
+    .min(1, { message: "Stock cannot be higher than 1" })
+    .max(100, { message: "Stock cannot be higher than 100" }),
+  price: z
+    .number()
+    .min(1, { message: "Price cannot be higher than 1" })
+    .max(100, { message: "Price cannot be higher than 20" }),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export function validate(body: FormData) {
+  return schema.safeParse(body);
+}
