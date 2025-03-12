1029587c73630b615613c29d79542f132cef2185
MohaK <mohakhalili@outlook.com>
2025-03-12 17:52:00 +0100

    added another localhost as origin

--- index.ts
+++ index.ts
@@ -13,6 +13,7 @@ app.use(
     origin: [
       "http://localhost:5589",
       "http://localhost:5173",
+      "http://localhost:5174",
       "http://localhost:4173",
       "https://foodventory-frontend.onrender.com",
     ],
